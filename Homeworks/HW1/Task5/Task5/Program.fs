e527f69dfaef9a7846f87e6c402de4d01baa20ac
Pavel <pavsaltykov@mail.ru>
2021-02-15 01:32:58 +0300

    implemented search in list

---
+++ Homeworks/HW1/Task5/Task5/Program.fs
@@ -0,0 +1,12 @@
+﻿let findInList list number =
+    let rec find list i =
+        match list with
+        | [] -> -1
+        | head :: tail ->
+            if List.head list = number then i
+            else find tail (i + 1)
+    find list 0
+
+let list = [1; 2; 3]
+let number = 1
+printfn "Position of %d in %A: %d" number list (findInList list number)
