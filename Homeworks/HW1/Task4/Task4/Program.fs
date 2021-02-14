47d4e9c91dc24d5723d111a655a736b4987878d8
Pavel <pavsaltykov@mail.ru>
2021-02-15 02:40:06 +0300

    implemented function

---
+++ Homeworks/HW1/Task4/Task4/Program.fs
@@ -0,0 +1,21 @@
+﻿let powers n m =
+    if n < 0 then invalidArg (nameof n) "Incorrect value"
+    if m < 0 then invalidArg (nameof m) "Incorrect value"
+
+    let power p =
+        let rec subPower acc count =
+            match count with
+            | 0 -> acc
+            | _ -> subPower (acc * 2) (count - 1)
+        subPower 1 p
+    let maxPower = power (n + m)
+
+    let rec makeList newList count =
+        match count with
+        | 0 -> newList
+        | _ -> makeList ((List.head newList / 2) :: newList) (count - 1)
+    makeList [maxPower] m
+
+let n = 2
+let m = 8
+powers n m |> printfn "Powers: %A"
