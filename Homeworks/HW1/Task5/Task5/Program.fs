1a612a162e8bbf589e18bef149b9855c6cb54bfc
Pavel <pavsaltykov@mail.ru>
2021-02-15 15:07:25 +0300

    corrected

--- Homeworks/HW1/Task5/Task5/Program.fs
+++ Homeworks/HW1/Task5/Task5/Program.fs
@@ -1,12 +1,14 @@
 ﻿let findInList list number =
     let rec find list i =
         match list with
-        | [] -> -1
-        | head :: tail ->
-            if List.head list = number then i
-            else find tail (i + 1)
+        | [] -> None
+        | head :: tail when head = number -> Some(i)
+        | head :: tail -> find tail (i + 1)
     find list 0
 
 let list = [1; 2; 3]
 let number = 1
-printfn "Position of %d in %A: %d" number list (findInList list number)
+let position = findInList list number
+match position with
+| Some(res) -> printfn "Position of %d in %A: %d" number list res
+| None -> printfn "%A doesn't contain %d" list number
