5db393d4e2aeefdbf0fb997f1c8105f85cf16953
Pavel <pavsaltykov@mail.ru>
2021-02-15 17:54:39 +0300

    added pown

--- Homeworks/HW1/Task4/Task4/Program.fs
+++ Homeworks/HW1/Task4/Task4/Program.fs
@@ -2,19 +2,11 @@
     if n < 0 then invalidArg (nameof n) "Incorrect value"
     if m < 0 then invalidArg (nameof m) "Incorrect value"
 
-    let power p =
-        let rec subPower acc count =
-            match count with
-            | 0 -> acc
-            | _ -> subPower (acc * 2) (count - 1)
-        subPower 1 p
-    let maxPower = power (n + m)
-
     let rec makeList newList count =
         match count with
         | 0 -> newList
         | _ -> makeList ((List.head newList / 2) :: newList) (count - 1)
-    makeList [maxPower] m
+    makeList [pown 2 (n + m)] m
 
 let n = 2
 let m = 8
