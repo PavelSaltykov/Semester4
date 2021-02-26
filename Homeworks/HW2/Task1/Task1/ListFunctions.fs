94194f552c5945d94b5d99c3c734078e9fad1b6a
Pavel <pavsaltykov@mail.ru>
2021-02-26 16:59:46 +0300

    implemented functions

---
+++ Homeworks/HW2/Task1/Task1/ListFunctions.fs
@@ -0,0 +1,11 @@
+﻿namespace Task1
+
+module ListFunctions =
+    let countEvenMap list =
+        list |> List.map (fun x -> (abs x + 1) % 2) |> List.sum
+
+    let countEvenFilter list =
+        list |> List.filter (fun x -> x % 2 = 0) |> List.length
+
+    let countEvenFold list =
+        List.fold (fun acc x -> (abs x + 1) % 2 + acc) 0 list
