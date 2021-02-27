04b636a834e2ca2de20a254d442aab5c5d71f71a
Pavel <pavsaltykov@mail.ru>
2021-02-27 21:59:28 +0300

    wrote comments

--- Homeworks/HW2/Task1/Task1/ListFunctions.fs
+++ Homeworks/HW2/Task1/Task1/ListFunctions.fs
@@ -1,11 +1,15 @@
 ﻿namespace Task1
 
 module ListFunctions =
+    
+    /// Counts even numbers in a list using the function List.map
     let countEvenMap list =
         list |> List.map (fun x -> (abs x + 1) % 2) |> List.sum
 
+    /// Counts even numbers in a list using the function List.filter
     let countEvenFilter list =
         list |> List.filter (fun x -> x % 2 = 0) |> List.length
 
+    /// Counts even numbers in a list using the function List.fold
     let countEvenFold list =
         List.fold (fun acc x -> (abs x + 1) % 2 + acc) 0 list
