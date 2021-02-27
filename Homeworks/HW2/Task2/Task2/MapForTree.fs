9a9990e88c9d0808058a93516000d3d1ace828a8
Pavel <pavsaltykov@mail.ru>
2021-02-28 01:31:33 +0300

    implemented map for tree

---
+++ Homeworks/HW2/Task2/Task2/MapForTree.fs
@@ -0,0 +1,11 @@
+﻿namespace Task2
+
+module MapForTree =
+    type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Empty
+    
+    let rec map func tree =
+        match tree with
+        | Empty -> Empty
+        | Node(value, left, right) -> Node(func value, map func left, map func right)
